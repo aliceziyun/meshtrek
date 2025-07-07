503320cd2886db751d4e5f2a80850b6997d67f17
Sissi <2225870865@qq.com>
2025-07-07 18:58:06 -0400

    update trace script

--- exper/envoy/trace_all.sh
+++ exper/envoy/trace_all.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 NAMESPACE="bookinfo"
+TRACE_SCRIPT="./envoy_http_trace.py"
 
 # get all pods in namespace
 PODS=$(kubectl get pods -n "$NAMESPACE" -o jsonpath='{.items[*].metadata.name}')
 
+for pod in $PODS; do
+    kubectl cp "$TRACE_SCRIPT" "$NAMESPACE/$pod:/tmp/envoy_http_trace.py" -c istio-proxy
+done
+
+wait
+
 # execute trace script in each pod
 for pod in $PODS; do
-  kubectl exec -i -n "$NAMESPACE" "$pod" -c istio-proxy -- sudo rm /tmp/trace_output.log
   echo "Running trace on pod: $pod"
-  kubectl exec -i -n "$NAMESPACE" "$pod" -c istio-proxy -- sudo python3 /home/envoy_http_trace.py &
+  kubectl exec -i -n "$NAMESPACE" "$pod" -c istio-proxy -- sudo python3 /tmp/envoy_http_trace.py &
 done
 
 wait

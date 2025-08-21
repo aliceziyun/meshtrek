9e0feb0b431099052e3bee62fb28e29fdb1dc188
Alice <ziyun_song@brown.edu>
2025-08-21 16:01:03 -0400

    update

--- exper/envoy/benchmark_trace.sh
+++ exper/envoy/benchmark_trace.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # do experiment
-NAMESPACE="hotel"
+NAMESPACE="bookinfo"
 
-# PRODUCTPAGE_IP=$(kubectl get service productpage -n "$NAMESPACE" -o jsonpath='{.spec.clusterIP}')
-# PRODUCTPAGE_PORT=$(kubectl get service productpage -n "$NAMESPACE" -o jsonpath='{.spec.ports[0].port}')
+PRODUCTPAGE_IP=$(kubectl get service productpage -n "$NAMESPACE" -o jsonpath='{.spec.clusterIP}')
+PRODUCTPAGE_PORT=$(kubectl get service productpage -n "$NAMESPACE" -o jsonpath='{.spec.ports[0].port}')
 
-PRODUCTPAGE_IP=$(kubectl get service frontend -n "$NAMESPACE" -o jsonpath='{.spec.clusterIP}')
-PRODUCTPAGE_PORT=5000
+# PRODUCTPAGE_IP=$(kubectl get service frontend -n "$NAMESPACE" -o jsonpath='{.spec.clusterIP}')
+# PRODUCTPAGE_PORT=5000
 REQUEST_URL="${PRODUCTPAGE_IP}:${PRODUCTPAGE_PORT}"
 
 echo "Request URL: $REQUEST_URL"
@@ -15,8 +15,8 @@ echo "Request URL: $REQUEST_URL"
 echo "Starting experiment..."
 RPS=60
 echo "Running RPS=$RPS..."
-# ./wrk2/wrk -t 10 -c 10 -d 60 -L http://$REQUEST_URL/productpage -R $RPS
-../wrk2/wrk -D exp -t 10 -c 20 -d 30 -L -s ./wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua http://$REQUEST_URL -R $RPS
+./wrk2/wrk -t 10 -c 10 -d 60 -L http://$REQUEST_URL/productpage -R $RPS
+# ../wrk2/wrk -D exp -t 10 -c 20 -d 30 -L -s ./wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua http://$REQUEST_URL -R $RPS
 
 wait
 

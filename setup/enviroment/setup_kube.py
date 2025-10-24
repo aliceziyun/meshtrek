c33f8c1f8c8df8a7857b809a7cf345b9925e3b42
Alice <ziyun_song@brown.edu>
2025-10-23 22:41:24 -0400

    Restructure setup directory

---
+++ setup/enviroment/setup_kube.py
@@ -0,0 +1,34 @@
+import re
+import json
+from shell_helper import ShellHelper
+
+class KubeSetUp:
+    def __init__(self, config_path):
+        self.shell_helper = ShellHelper(config_path)
+
+    def environment_setup(self):
+        self.shell_helper.execute_parallel("./kube/kube.sh", mode=0)
+
+    def init_kubernetes_on_main(self):
+        print("[*] Initializing Kubernetes on main node...")
+        config = self.shell_helper.config
+        result = self.shell_helper.execute_script(0, config["nodes"][0], config["nodes_home"], config["nodes_user"], "./kube/init_kube.sh")
+        match = re.search(r"(kubeadm join\s[\s\S]+?)(?:\n\n|\Z)", str(result))
+        if match:
+            join_command = match.group(1)
+        else:
+            print("[!] Failed to extract join command from kubeadm output.")
+            exit(1)
+        return join_command
+
+    def join_workers_to_cluster(self, join_command):
+        # write join command to file
+        with open("./kube/join_kube.sh", "w") as f:
+            f.write(f"sudo {join_command}")
+
+        self.shell_helper.execute_parallel("./kube/join_kube.sh", mode=1)
+
+    def kube_cluster_setup(self):
+        self.environment_setup()
+        join_command = self.init_kubernetes_on_main()
+        self.join_workers_to_cluster(join_command)
--- setup/tmp.sh
+++
@@ -1,2 +0,0 @@
-sudo kubeadm join 130.127.133.159:6443 --token vedko1.ja4irtwihka52bgc \
-        --discovery-token-ca-cert-hash sha256:ad317227b191e38a02c2eb60466b390c8d46c85a52d46ec1d040ecaf965e5606 

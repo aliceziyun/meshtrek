14099ba86ebe1d958ea74404417300941816af14
Alice <ziyun_song@brown.edu>
2025-10-26 19:38:06 -0400

    bug fix

--- setup/environment/setup_kube.py
+++ setup/environment/setup_kube.py
@@ -1,20 +1,24 @@
 import re
+import os
 import json
 from shell_helper import ShellHelper
 
 class KubeSetUp:
     def __init__(self, config_path):
         self.shell_helper = ShellHelper(config_path)
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
 
     def environment_setup(self):
         print("[*] Setting up Kubernetes environment on all nodes...")
-        self.shell_helper.execute_parallel("./kube/kube.sh", mode=0)
+        setup_script_path = os.path.join(self.current_dir, "./kube/kube.sh")
+        self.shell_helper.execute_parallel(setup_script_path, mode=0)
 
     def init_kubernetes_on_main(self):
         print("[*] Initializing Kubernetes on main node...")
         config = self.shell_helper.config
-        self.shell_helper.copy_files_to_nodes("./kube/init_kube.sh", mode=2)
-        result = self.shell_helper.execute_script(0, config["nodes"][0], config["nodes_home"], config["nodes_user"], "./kube/init_kube.sh")
+        init_script_path = os.path.join(self.current_dir, "./kube/init_kube.sh")
+        self.shell_helper.copy_files_to_nodes(init_script_path, mode=2)
+        result = self.shell_helper.execute_script(0, config["nodes"][0], config["nodes_home"], config["nodes_user"], init_script_path)
         match = re.search(r"(kubeadm join\s[\s\S]+?)(?:\n\n|\Z)", str(result))
         join_command = ""
         if match:
@@ -27,12 +31,14 @@ class KubeSetUp:
 
     def join_workers_to_cluster(self, join_command):
         print("[*] Joining worker nodes to the Kubernetes cluster...")
+        join_kube_path = os.path.join(self.current_dir, "./kube/join_kube.sh")
+        after_join_path = os.path.join(self.current_dir, "./kube/after_join.sh")
         # write join command to file
-        with open("./kube/join_kube.sh", "w") as f:
+        with open(join_kube_path, "w") as f:
             f.write(f"sudo {join_command}")
 
-        self.shell_helper.execute_parallel("./kube/join_kube.sh", mode=1)
-        self.shell_helper.execute_parallel("./kube/after_join.sh", mode=2)
+        self.shell_helper.execute_parallel(join_kube_path, mode=1)
+        self.shell_helper.execute_parallel(after_join_path, mode=2)
 
     def kube_cluster_setup(self):
         self.environment_setup()
@@ -40,5 +46,6 @@ class KubeSetUp:
         self.join_workers_to_cluster(join_command)
 
 if __name__ == "__main__":
-    kube_setup = KubeSetUp("./config.json")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    kube_setup = KubeSetUp(os.path.join(current_dir, "./config.json"))
     kube_setup.kube_cluster_setup()

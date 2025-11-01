36499b2b671213c22730a2b74bad578bbd0c03ca
Alice <ziyun_song@brown.edu>
2025-10-31 23:52:01 -0400

    update

--- setup/environment/kube/join_kube.sh
+++ setup/environment/kube/join_kube.sh
@@ -1,2 +1,2 @@
-sudo kubeadm join 130.127.133.110:6443 --token rlf9i7.0qcnbmik6xyowhse \
-        --discovery-token-ca-cert-hash sha256:096be5b34f707e81d1bc31f0d938c8d0b89506fcaebfd8e5789a158691f0c9fc 
+sudo kubeadm join 130.127.133.161:6443 --token nlht9n.g2xx8xvumqnyna6l \
+	--discovery-token-ca-cert-hash sha256:d8915bb8adb90c9a1f5d9d28191f4b12d052f44b23d11b53efc26ae52bafc648

79cc1421a0025726ee0fd3459057d080103debcc
Alice <ziyun_song@brown.edu>
2025-11-04 15:55:14 -0500

    four nodes

--- exper/metric/script/fix_schedule_social.sh
+++ exper/metric/script/fix_schedule_social.sh
@@ -9,13 +9,22 @@ fi
 
 base_dir="$HOME/meshtrek/resources/benchmark/SocialNetwork/helm-chart/socialnetwork/charts"
 
-group_one=("compose-post-service" "home-timeline-service" "home-timeline-redis" "media-frontend" \
+# group_one=("compose-post-service" "home-timeline-service" "home-timeline-redis" "media-frontend" \
+#            "media-memcached" "media-mongodb" "media-service")
+# group_two=("nginx-thrift" "post-storage-memcached" "post-storage-mongodb" "post-storage-service" \
+#            "social-graph-mongodb" "social-graph-redis" "social-graph-service")
+# group_three=("text-service" "unique-id-service" "url-shorten-memcached" "url-shorten-mongodb" \
+#              "url-shorten-service" "user-memcached" "user-mongodb" "user-mention-service" "user-service" \
+#              "user-timeline-mongodb" "user-timeline-redis" "user-timeline-service")
+
+group_one=("home-timeline-service" "home-timeline-redis" "media-frontend" \
            "media-memcached" "media-mongodb" "media-service")
-group_two=("nginx-thrift" "post-storage-memcached" "post-storage-mongodb" "post-storage-service" \
+group_two=("post-storage-memcached" "post-storage-mongodb" "post-storage-service" \
            "social-graph-mongodb" "social-graph-redis" "social-graph-service")
 group_three=("text-service" "unique-id-service" "url-shorten-memcached" "url-shorten-mongodb" \
-             "url-shorten-service" "user-memcached" "user-mongodb" "user-mention-service" "user-service" \
              "user-timeline-mongodb" "user-timeline-redis" "user-timeline-service")
+group_four=("url-shorten-service" "user-memcached" "user-mongodb" "user-mention-service" "user-service" \
+            "jaeger" "compose-post-service" "nginx-thrift")
 
 patch_group() {
   local group_name="$1"
@@ -43,5 +52,6 @@ patch_group() {
 patch_group "group_one"   "${nodes[0]}" "${group_one[@]}"
 patch_group "group_two"   "${nodes[1]}" "${group_two[@]}"
 patch_group "group_three" "${nodes[2]}" "${group_three[@]}"
+patch_group "group_four"  "${nodes[3]}" "${group_four[@]}"
 
 echo "Scheduling completed. Please check your values.yaml files."

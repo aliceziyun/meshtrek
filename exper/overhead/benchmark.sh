4c12935869ae57efd80c83b40655d54965f4644f
Alice <ziyun_song@brown.edu>
2025-11-04 14:56:26 -0500

    probably enough for social media

--- exper/overhead/benchmark.sh
+++ exper/overhead/benchmark.sh
@@ -22,8 +22,25 @@ benchmark_hotel() {
     wait
 }
 
+benchmark_social() {
+    NAMESPACE="social"
+    local ip=$(kubectl get service nginx -n "$NAMESPACE" -o jsonpath='{.spec.clusterIP}')
+    local port=8080
+    local request_url="${ip}:${port}/wrk2-api/post/compose"
+    OUTPUT_FILE="social_benchmark_results.txt"
+
+    echo "Benchmarking Social Network Service" | tee -a "$OUTPUT_FILE"
+    echo "Threads: $THREADS, Connections: $CONNECTIONS, Target RPS: $TARGET_RPS, Duration: $DURATION seconds" | tee -a "$OUTPUT_FILE"
+
+    ~/DeathStarBench/wrk2/wrk -D exp -t "$THREADS" -c "$CONNECTIONS" -d "$DURATION" -L ~/meshtrek/resources/benchmark/SocialNetwork/wrk2/mixed_workload.lua "http://$request_url" -R "$TARGET_RPS"
+
+    wait
+}
+
 if [ "$MICRO_SERVICE" == "hotel" ]; then
     benchmark_hotel
+elif [ "$MICRO_SERVICE" == "social" ]; then
+    benchmark_social
 else
     echo "Unsupported microservice: $MICRO_SERVICE"
     exit 1

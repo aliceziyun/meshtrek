1072d48e07df57a0a1ba7a25fa0cd08b6b1d7d41
Alice <ziyun_song@brown.edu>
2025-11-10 13:32:01 -0500

    update ambient setup

---
+++ exper/environment/ambient/ambient.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# First we still need to download Istio
+cd ~
+export ISTIO_VERSION=1.26.0
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
+cd istio-1.26.0
+export PATH=$PWD/bin:$PATH
+
+kubectl create namespace hotel
+kubectl label namespace hotel istio.io/dataplane-mode=ambient
+
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v1.4.0" | kubectl apply -f -; }
+
+istioctl install --set profile=ambient --set values.pilot.env.PILOT_ENABLE_GATEWAY_API=true -y
+
+istioctl waypoint generate --for service -n hotel
+istioctl waypoint apply -n hotel --enroll-namespace

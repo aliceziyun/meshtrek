04ed3141fed37634c504e562665d95824b51e485
heesuch <juliehsuchung@gmail.com>
2025-11-28 00:48:25 -0500

    feat: added envoyfilter policy and opa policy

--- exper/environment/kube/join_kube.sh
+++ exper/environment/kube/join_kube.sh
@@ -1,2 +1,2 @@
-sudo kubeadm join 130.127.133.161:6443 --token jdc9fe.2wp9wxfsax8cmc1v \
-	--discovery-token-ca-cert-hash sha256:2a8db669da468b7131cd76c77a14aa6abb509f53cf8b9041a8505bb472db1961
+sudo kubeadm join 128.110.217.158:6443 --token 1i0qc3.opynyvgr24tk7037 \
+	--discovery-token-ca-cert-hash sha256:5ae9a6777dc5f9995268a41d7bc9c791ea3369addce2462460419a14e751d999

9c7e4580d1cfbf01719a9137a8f396057d1b15a0
Alice <77952315+aliceziyun@users.noreply.github.com>
2025-12-31 21:09:55 -0600

    refractor: delete unnecessary files in the branch

--- exper/environment/istio/istio.sh
+++ exper/environment/istio/istio.sh
@@ -8,46 +8,9 @@ curl -L https://istio.io/downloadIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
 cd istio-1.26.0
 export PATH=$PWD/bin:$PATH
 # install service mesh for bookinfo application
-istioctl install -f samples/bookinfo/demo-profile-no-gateways.yaml -y
+istioctl install -f ~/meshtrek/resources/istio_config/no_resource_limit.yaml -y
 kubectl create namespace $NAMESPACE
 kubectl label namespace $NAMESPACE istio-injection=enabled
-
-# download k8s gateway api
-# kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
-# { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v1.2.1" | kubectl apply -f -; }
-
-# download jaeger
-# kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/jaeger.yaml
-# cat <<EOF > ./tracing.yaml
-# apiVersion: install.istio.io/v1alpha1
-# kind: IstioOperator
-# spec:
-#   meshConfig:
-#     enableTracing: true
-#     defaultConfig:
-#       tracing: {} # disable legacy MeshConfig tracing options
-#     extensionProviders:
-#     - name: jaeger
-#       opentelemetry:
-#         port: 4317
-#         service: jaeger-collector.istio-system.svc.cluster.local
-# EOF
-# istioctl install -f ./tracing.yaml --skip-confirmation
-
-# kubectl apply -f - <<EOF
-# apiVersion: telemetry.istio.io/v1
-# kind: Telemetry
-# metadata:
-#   name: mesh-default
-#   namespace: istio-system
-# spec:
-#   tracing:
-#   - providers:
-#     - name: jaeger
-# EOF
-
-# patch services so we can access jaeger from outside
-# kubectl patch svc tracing -n istio-system -p '{"spec":{"type":"NodePort"}}'
 
 # download wrk2 for performance test
 sudo apt install libssl-dev

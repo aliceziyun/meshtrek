cecb6416bfc86c429f352e09564618aff9862159
Alice <ziyun_song@brown.edu>
2026-01-31 11:31:36 -0500

    refractor: delete unused files

--- exper/metric/script/reset_database_for_hotel.sh
+++
@@ -1,2 +0,0 @@
-sudo rm -r /data/volumes
-sudo mkdir -p /data/volumes
--- resources/benchmark/TrainTicket/launch_train_ticket_worker.sh
+++
@@ -1,7 +0,0 @@
-sudo modprobe dm-snapshot
-sudo modprobe nvme_tcp
-sudo modprobe iscsi_tcp
-echo "dm-snapshot" | sudo tee -a /etc/modules-load.d/dm-snapshot.conf
-
-sudo mkdir -p /mnt/openebs
-sudo mount /dev/sda3 /mnt/openebs

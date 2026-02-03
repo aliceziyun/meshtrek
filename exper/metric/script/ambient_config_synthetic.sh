7b61d8f005fc51690402238b66ea5d7f32775f6d
Alice <ziyun_song@brown.edu>
2026-02-02 23:20:55 -0500

    feat: add bind each node and remote

--- exper/metric/script/ambient_config_synthetic.sh
+++ exper/metric/script/ambient_config_synthetic.sh
@@ -14,70 +14,85 @@ delete() {
   sleep 10
 }
 
-apply_each_service() {
-  node1_services=("search")
-  node2_services=("profile" "rate" "user")
-  node3_services=("geo" "reservation")
-  node4_services=("frontend" "recommendation")
-
+apply_each_node() {
   nodes=($(kubectl get nodes --no-headers | awk '!/control-plane/ {print $1}'))
-
-  for i in {1..4}; do
-    node="${nodes[$((i-1))]}"
-    services_arr="node${i}_services[@]"
-
-    for svc in "${!services_arr}"; do
-      waypoint="waypoint-${svc}"
-
-      echo "Applying waypoint [$waypoint] for service [$svc] on node [$node]"
-
-      istioctl waypoint apply -n default --name "$waypoint"
-
-      sleep 1
-
-      kubectl patch deploy "$waypoint" -n default --type=json \
-      -p='[
-        {
-          "op": "add",
-          "path": "/spec/template/spec/nodeSelector",
-          "value": { "kubernetes.io/hostname": "'"$node"'" }
-        }
-      ]'
-
-      echo "Patched waypoint [$waypoint] to node [$node]"
-    done
+  waypoints=("waypoint1" "waypoint2" "waypoint3" "waypoint4")
+  for waypoint in "${waypoints[@]}"; do
+    istioctl waypoint apply -n hotel --name "$waypoint"
   done
 
-  sleep 5
+  for i in "${!nodes[@]}"; do
+    node="${nodes[$i]}"
+    waypoint="${waypoints[$i]}"
+
+    kubectl patch deploy "$waypoint" -n hotel --type=json \
+    -p='[
+      {
+        "op": "add",
+        "path": "/spec/template/spec/nodeSelector",
+        "value": { "kubernetes.io/hostname": "'"$node"'" }
+      }
+    ]'
+    echo "Patched waypoint [$waypoint] to node [$node]"
+  done
 }
 
-bind_each_service() {
+bind_each_node() {
+  nodes=($(kubectl get nodes --no-headers | awk '!/control-plane/ {print $1}'))
+  waypoints=("waypoint1" "waypoint2" "waypoint3" "waypoint4")
+
   node1_services=("search")
   node2_services=("profile" "rate" "user")
   node3_services=("geo" "reservation")
   node4_services=("frontend" "recommendation")
 
-  for i in {1..4}; do
-    services_arr="node${i}_services[@]"
-
-    for svc in "${!services_arr}"; do
-      waypoint="waypoint-${svc}"
-      kubectl label service "$svc" -n default istio.io/use-waypoint="$waypoint" --overwrite
-      echo "Labeled service [$svc] to use waypoint [$waypoint]"
-    done
+  for i in "${!nodes[@]}"; do
+      waypoint="${waypoints[$i]}"
+      services_var="node$((i+1))_services[@]"
+      for svc in "${!services_var}"; do
+          kubectl label service "$svc" -n hotel istio.io/use-waypoint="$waypoint"
+          echo "Labeled service [$svc] to use waypoint [$waypoint]"
+      done
   done
-
-  bind_frontend
 
   sleep 5
 }
+
+bind_each_node_remote() {
+  nodes=($(kubectl get nodes --no-headers | awk '!/control-plane/ {print $1}'))
+  waypoints=("waypoint1" "waypoint2" "waypoint3" "waypoint4")
+
+  node1_services=("search")
+  node2_services=("profile" "rate" "user")
+  node3_services=("geo" "reservation")
+  node4_services=("frontend" "recommendation")
+
+  num_nodes=${#nodes[@]}
+
+  for i in "${!nodes[@]}"; do
+      wp_index=$(( (i + 1) % num_nodes ))
+      waypoint="${waypoints[$wp_index]}"
+
+      services_var="node$((i+1))_services[@]"
+      for svc in "${!services_var}"; do
+          kubectl label service "$svc" -n hotel istio.io/use-waypoint="$waypoint" --overwrite
+          echo "Labeled service [$svc] on node$((i+1)) to use waypoint [$waypoint]"
+      done
+  done
+
+  sleep 5
+}
+
+
 
 if [ "$1" == "delete" ]; then
   delete
-elif [ "$1" == "apply_each_service" ]; then
-  apply_each_service
+elif [ "$1" == "apply_each_node" ]; then
+  apply_each_node
 elif [ "$1" == "bind_each_service" ]; then
-  bind_each_service
+  bind_each_node
+elif [ "$1" == "bind_each_service_remote" ]; then
+  bind_each_node_remote
 else
   echo "Usage"
   exit 1
